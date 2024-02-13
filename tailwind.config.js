2a87be9df70396ae5e3f7f8837cfae185a4f35ea
salehmb1999 <salehmirbagheri78@gmail.com>
2024-02-13 18:43:40 +0330

    initializing the project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      "poppins-extraLight": "poppins-extraLight",
+      "poppins-light": "poppins-light",
+      "poppins-regular": "poppins-regular",
+      "poppins-medium": "poppins-medium",
+      "poppins-semiBold": "poppins-semiBold",
+      "poppins-bold": "poppins-bold",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
